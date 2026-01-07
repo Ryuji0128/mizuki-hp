773354d675ebc18fa582fb3a7ff2d8be53281c3c
Ryuji0128 <setaseisakusyo@gmail.com>
2026-01-07 13:51:53 +0000

    赤文字修正

--- src/app/discription/Message02.tsx
+++ src/app/discription/Message02.tsx
@@ -4,20 +4,17 @@ export default function Message01() {
   return (
     <Box sx={{ maxWidth: 800, mx: "auto", lineHeight: 0.8 }}>
       <Typography variant="body1" lineHeight={2.0}>
-        当院では、「かかりつけ医」機能を有する診療所として機能強化加算を算定しております。<br></br>
+        患者の皆様へ<br></br>
       </Typography>
       <List sx={{ listStyleType: "disc", pl: 4 }}>
         <ListItem sx={{ display: "list-item" }}>
-          健康診断の結果などの健康管理に係る相談に応じます。
+          当院はマイナンバーカードによるオンライン資格確認を行なっています。
         </ListItem>
         <ListItem sx={{ display: "list-item" }}>
-          保健・福祉サービスの利用等に関する相談に応じます。
+          オンライン資格確認により診療情報を取得・活用し、質の高い医療の提供に努めています
         </ListItem>
         <ListItem sx={{ display: "list-item" }}>
-          訪問診療を行なっている患者様に対し、夜間・休日の問合せへの対応を行います。
-        </ListItem>
-        <ListItem sx={{ display: "list-item" }}>
-          必要に応じて、専門医・専門医療機関を紹介します。
+          正確な情報を取得するために・活用するために、マイナ保険証の利用にご協力をお願い致します。
         </ListItem>
       </List>
     </Box>
